71d60666984147a2e4f97d51400628029de54b71
Pacifiky <87620151+infinitypacific@users.noreply.github.com>
2024-08-13 17:17:09 -0700

    Added V2

---
+++ V2/script.js
@@ -0,0 +1,125 @@
+let imgs = ["https://media.discordapp.net/attachments/1087427711175233687/1260036953391235093/CarThing.png?ex=668ddc5c&is=668c8adc&hm=066d7b80875eb6946cd490002b651cdfc8001e9c79954a76b4ef4a3c145b29c7&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260036957350789134/moodydramatic2.png?ex=668ddc5c&is=668c8adc&hm=ef0ea0cafc150b25b54a68b3f0506c34ca47f7405b1431ef7f8071c3a0e8814e&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260036956033912852/GlassExplodeBackgroundFinal.png?ex=668ddc5c&is=668c8adc&hm=1a319f4c2154418100c70499404287ddb177d4d068b17dd9153260a808798c0d&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260037139887034438/ChristmasvFinal1.png?ex=668ddc88&is=668c8b08&hm=eb6ebb92a6b914d3b6118b3ce281f52c6b9c83bad48fc4be45c303077b34e0b4&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260036952632066170/carthing3.png?ex=668ddc5b&is=668c8adb&hm=525b4c0347eee3275eb89b49db65112d86fddb80ba2db17853969b7cc9994111&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260036954787942441/CurrieWatermark.png?ex=668ddc5c&is=668c8adc&hm=b2af40f40e222a40b2371c7f574607225b36ef6d3031819a01860ccb3f29ca8f&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260036955505164442/GunsComposite2.png?ex=668ddc5c&is=668c8adc&hm=c8af768bb56e7e07b2d72224648e2f2db3b6830348fd36035cb37b87ae0f4dac&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260036958189781114/FogGlowAndSmokev2.png?ex=668ddc5d&is=668c8add&hm=3f31511f512575f2a38de7f8c5475a74f2f9b7d86a46d9b483bc463bb0d59330&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260037141338263675/testrenderFHD3.png?ex=668ddc88&is=668c8b08&hm=0cb4c5a8f2ef49c191a0d57958ea3f8972eb92ef75c41956e657195e09b40ace&=&format=webp&quality=lossless&width=640&height=360",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260036956776173688/PapersDark.png?ex=668ddc5c&is=668c8adc&hm=96938f8a981c2d29604935da384a75354ff6b573e9169f3f7d7aaa81f192874b&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260083680320426034/Reindeer.png?ex=668e07e0&is=668cb660&hm=daa6f0788614c9b8bb2a9126d159a84caf4550d48fcb5d11ed0e71e6cce445af&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260083681075134464/tophat.png?ex=668e07e0&is=668cb660&hm=eef0bafd6020c830e970f56327f5f995dcd137317d3d9e8e59f96ca68ffd333e&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1155506838440247397/Bokehafter.png?ex=668d451a&is=668bf39a&hm=d17846410e9c2676fc1a24c541ac5ce61b4291c30c4d79889146bf9489238935&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1155506834459852870/Sci-FiWatermark.png?ex=668d4519&is=668bf399&hm=768191eb6aec545aad0fdee594d5d11a907480e22fad22979dff1b5b1572d7ca&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260084413786619915/darkenedwallpaper.png?ex=668e088f&is=668cb70f&hm=c0fe7093ab1abee2c1c46eb40f83961fe4c595a391ba0863672538566d6e925f&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1088609587021762569/Teacup.png?ex=668dcbae&is=668c7a2e&hm=4a0cbe1da64657f222549d0f24a16b346d7d08ada16dccef9e277e6a09fe8b37&=&format=webp&quality=lossless&width=275&height=154",
+    "https://media.discordapp.net/attachments/1087427711175233687/1088236644496330752/HammerRoblox2.png?ex=668dc1da&is=668c705a&hm=547fba2b04373e119e24abbc988fbd6e3f8efa4d134c82297ff6c1feec65f411&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260085709595873392/1kthumbmc_1.png?ex=668e09c4&is=668cb844&hm=a06c944c23c2dd424ee9042713c69c7da56f04472a828ff8739a09d69b6fe1d1&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260085710342590474/finishedproductmh.png?ex=668e09c4&is=668cb844&hm=dc679319771f6926fe7923a09202b166d3eefc08155005b05540b005dfa51caf&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260085710782857327/backrooms.jpg?ex=668e09c4&is=668cb844&hm=41b58746c68770e0e2b7fb10fc33bc29f21b518ff3220a9cad9ffee8553cdc38&=&format=webp&width=640&height=360",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260085711173058600/backroom2.png?ex=668e09c4&is=668cb844&hm=df58da65b209acd8ded3f6d5288c1042828129f42268e9545c17264849953d61&=&format=webp&quality=lossless&width=640&height=360",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260085712284287059/New_Project_51.png?ex=668e09c5&is=668cb845&hm=53899a59865e9af5acfee0440145f5adf0bbe7e7b7cb48fe88a15709af92b031&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260085713282666507/kirkland.png?ex=668e09c5&is=668cb845&hm=e96bf9e59521cd242dd2f2cd164a4b7eee587dbbd5be08bfbc577eb413a2e31f&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260085714012340224/asfqwasgfewads.png?ex=668e09c5&is=668cb845&hm=cd96e4c59ddff829898bc5cd6864c3e5f0174afa2ba53ada82c103072087fecc&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260085714951999629/Thumb2.png?ex=668e09c5&is=668cb845&hm=167e8731c318ce597b6e3cb2a9e8a7339cc0f8a353041be953110a24767c2dfe&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260085715497259068/thumbn.png?ex=668e09c5&is=668cb845&hm=795e42e7490480a4b5802d11141d225271c6743965e0d258360f2500277ad989&=&format=webp&quality=lossless&width=640&height=360",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260086175876776017/steamp_motion_blur.png?ex=668e0a33&is=668cb8b3&hm=272ef1dd06dba7ddcce5e71f74481f12dff0b539c0610945e431f989e444e322&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260086173884350524/couchmed_1.png?ex=668e0a33&is=668cb8b3&hm=07c73218a43ec58cad5829dc0eeb8cb252185cb2f48b069f81c1fe729537b7eb&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260086175016685649/candleey.png?ex=668e0a33&is=668cb8b3&hm=4c9d2c01e5c7729e343149c149d9aa09e64b66afa646a7141d2efd28903eaef4&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260086176740540426/WildWest.png?ex=668e0a33&is=668cb8b3&hm=695445d7f966ff67b9f5e46d9e5567252228c0e758a94ca1a4a712ba79325cf7&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260086177420148906/jonas.png?ex=668e0a33&is=668cb8b3&hm=a0cabd8bc0111f05542f0c9998f10ec2281613f7eab9f7b2bafe378b93483b3d&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260086178074595399/field.png?ex=668e0a34&is=668cb8b4&hm=fcb25a564a94622dd76eab47b67057c67522cdb0de6ebdd33711750a592efac6&=&format=webp&quality=lossless&width=640&height=360",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260086178590228530/ren2.png?ex=668e0a34&is=668cb8b4&hm=7c02022a1e33e5459f5f57e654414b498f23df82aafef8c28b3ccd284282bfa5&=&format=webp&quality=lossless&width=960&height=540",
+    "https://media.discordapp.net/attachments/1087427711175233687/1260086180075278377/untiasdtled.png?ex=668e0a34&is=668cb8b4&hm=811b67a70d54f99b3574fe80907255db84098a02abb5d844ae3ff380bbc0e692&=&format=webp&quality=lossless&width=960&height=540"];
+let captions = ['"City Car"<br/>1/31/24<br/>made for Chunky Guns',
+    '"Campfire"<br/>1/1/24',
+    '"Shattered"<br/>1/6/24',
+    '"2023 Christmas"<br/>12/30/23',
+    '"Desert Car"<br/>2/1/24<br/>made for Chunky Guns',
+    '"Currie"<br/>1/6/24<br/>made for CurrieSandwich',
+    '"Guns"<br/>1/7/24',
+    '"Fog-Glow"<br/>1/1/24',
+    '"Gift Preview"<br/>11/25/23',
+    '"Papers"<br/>1/5/24',
+    '"Lit up"<br/>12/28/23',
+    '"Top hat"<br/>12/27/23',
+    '"Bokeh"<br/>9/24/23',
+    '"Sci-Fi"<br/>9/23/23',
+    '"Cosmos"<br/>8/12/23',
+    '"Teacup"<br/>3/23/23',
+    '"Hammer"<br/>3/22/23',
+    '"1k Subs!"<br/>9/2/23<br/>Thumbnail concept',
+    '"Resting"<br/>7/18/23<br/>Comission',
+    '"After the backrooms"<br/>6/7/23<br/>Thumbnail',
+    '"Wet Backrooms"<br/>6/3/23<br/>Test render',
+    '"Fake bottle"<br/>6/3/23<br/>CGI test',
+    '"Kirkland"<br/>6/3/23',
+    '"Sky"<br/>5/14/23',
+    '"Goodbye Game boy 600"<br/>4/10/23<br/>Thumbnail',
+    '"Fireworks"<br/>4/10/23<br/>Test Render',
+    '"Steampunk"<br/>2/22/23',
+    '"Cozy home"<br/>10/28/23',
+    '"Candley"<br/>6/5/23<br/>Comission rig test',
+    '"Wild West"<br/>2/14/23<br/>Comission',
+    '"War"<br/>2/11/23<br/>Comission',
+    '"Field"<br/>2/11/23',
+    '"Bored"<br/>1/29/23',
+    '"New Skin"<br/>1/21/23',];
+let list = document.getElementById("imagelist");
+let slist = document.getElementById("squarelist");
+
+window.addEventListener("scroll", (e) => {
+    for (let i = 0; i < document.getElementsByClassName("scr").length; i++) {
+        if (document.getElementsByClassName("scr")[i].getBoundingClientRect().top + window.innerHeight / 4 <= window.innerHeight) {
+            document.getElementsByClassName("scr")[i].classList.remove("hid");
+            document.getElementsByClassName("scr")[i].classList.add("sho");
+        }
+        else if (document.getElementsByClassName("scr")[i].getBoundingClientRect().top + window.innerHeight / 4 > window.innerHeight) {
+            document.getElementsByClassName("scr")[i].classList.remove("sho");
+            document.getElementsByClassName("scr")[i].classList.add("hid");
+        }
+    }
+})
+
+for (let i = 0; i < imgs.length; i++) {
+    let nimg = document.createElement("img");
+    nimg.className = "scr hid wcard";
+    nimg.setAttribute("src", imgs[i]);
+    //console.log(imgs[i]);
+    nimg.addEventListener("mouseleave", () => {
+        nimg.style = "z-index: 2";
+        setTimeout(() => { nimg.style = "" }, 500)
+    })
+    list.appendChild(nimg);
+    let simg = document.createElement("div");
+    let scap = document.createElement("div");
+    simg.className = "scr hid img"
+    simg.style = "background: url(" + imgs[i] + ");";
+    scap.className = "imgcap hid";
+    scap.innerHTML = captions[i];
+    simg.addEventListener("mouseover", () => {
+        scap.classList.remove("hid");
+        scap.classList.add("sho");
+    })
+    simg.addEventListener("mouseleave", () => {
+        scap.classList.remove("sho");
+        scap.classList.add("hid");
+    })
+    simg.appendChild(scap);
+    slist.appendChild(simg);
+}
+
+for (let i = 0; i < document.getElementsByClassName("scr").length; i++) {
+    if (document.getElementsByClassName("scr")[i].getBoundingClientRect().top + window.innerHeight / 4 <= window.innerHeight) {
+        document.getElementsByClassName("scr")[i].classList.remove("hid");
+        document.getElementsByClassName("scr")[i].classList.add("sho");
+    }
+}
+
+for (let i = 0; i < document.getElementsByClassName("wcard").length; i++) {
+    document.getElementsByClassName("wcard")[i].addEventListener("mouseleave", () => {
+        document.getElementsByClassName("wcard")[i].style = "z-index: 2";
+        setTimeout(() => { document.getElementsByClassName("wcard")[i].style = "" }, 500)
+    })
+}
